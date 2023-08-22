ed8ed256841b082dc1413c190a46fb01c7246617
alrhazes <anuuman2@gmail.com>
2023-08-22 12:59:15 +0100

    few updates

--- lib/screens/onboard_screen/controller/onboard_controller.dart
+++ lib/screens/onboard_screen/controller/onboard_controller.dart
@@ -2,14 +2,12 @@ import 'package:com_policing_incident_app/screens/onboard_screen/model/onboard_m
 
 class OnboardController {
   List<OnboardModel> screens = [
-    OnboardModel('assets/images/image_1.png', 'Report Crime On The Go ',
+    OnboardModel('assets/images/com_1.png', 'Report Crime On The Go ',
         'Have Access to Full Justice Support'),
-    OnboardModel(
-        'assets/images/civil-right.png',
-        'Send an SOS for an Emergency',
+    OnboardModel('assets/images/com_2.png', 'Send an SOS for an Emergency',
         'Send an SOS for Emergency To alert the police units and Emergency Contacts'),
     OnboardModel(
-        'assets/images/image_2.png',
+        'assets/images/com_3.png',
         'Have Access To Limited Security Features',
         'Locate The nearest police in your community if you feel unsafe'),
   ];
